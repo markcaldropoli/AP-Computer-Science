6abee195191936961b36d3ab29847b537a640e7a
Mark Caldropoli <mcaldro1@binghamton.edu>
2016-09-15 21:05:16 -0400

    Create Exercise9.java

---
+++ Chapter-2/Exercise9.java
@@ -0,0 +1,14 @@
+public class Exercise9
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println("  xxxxx ");
+    System.out.println("  x   x ");
+    System.out.println("(( o o )) ");
+    System.out.println(" |  V  | ");
+    System.out.println(" | === | ");
+    System.out.println("  ----- ");
+  }
+
+}
