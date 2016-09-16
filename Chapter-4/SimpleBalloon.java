2f6820927eb9e7403070be594c17f0ce70598132
Mark Caldropoli <mcaldro1@binghamton.edu>
2016-09-15 21:18:29 -0400

    Create SimpleBalloon.java

---
+++ Chapter-4/SimpleBalloon.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+
+public class SimpleBalloon
+{
+  
+  public static void main(String[] args)
+  {
+    Balloon b = new Balloon(100,100,20,Color.RED);
+    System.out.println(b);
+  }
+    
+}
