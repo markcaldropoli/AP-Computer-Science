19647d2d6fb3b9417cd09d6afaca256f32d3296d
Mark Caldropoli <mcaldro1@binghamton.edu>
2016-09-15 21:34:40 -0400

    Create Exercise1.java

---
+++ Chapter-8/Exercise1.java
@@ -0,0 +1,11 @@
+
+public class Exercise1
+{
+  
+  public void test() {
+    //Add a double \ to prevent escape keys
+ //   String fileName = "C:\dictionaries\words.txt";
+ //   String fileName = "C:\\dictionaries\\words.txt";
+  }
+  
+}
