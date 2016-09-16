c4abad50541cbf0d74efb4504b715defcc13e717
Mark Caldropoli <mcaldro1@binghamton.edu>
2016-09-15 21:18:51 -0400

    Create SquareBalloon.java

---
+++ Chapter-4/SquareBalloon.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class SquareBalloon extends Balloon
+{
+  public SquareBalloon()
+  { }
+  
+  public SquareBalloon(int x, int y, int r, Color c)
+  {
+    super(x,y,r,c);
+  }
+}
