79dbe49b07cde80aff21e45d2b9c11f09aa01817
Mark Caldropoli <mcaldro1@binghamton.edu>
2016-09-15 21:20:28 -0400

    Create Exercise7.java

---
+++ Chapter-5/Exercise7.java
@@ -0,0 +1,10 @@
+public class Exercise7
+{
+  public static void main(String[] args) {
+    System.out.println(5/10);
+    System.out.println(1/2*10);
+    System.out.println(1.0/2*10);
+    System.out.println(1/2.0*10);
+    System.out.println(13%5);
+  }
+}
