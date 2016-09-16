bdf14dc73242b76b5c3e7ec74320def369a9220a
Mark Caldropoli <mcaldro1@binghamton.edu>
2016-09-15 21:07:00 -0400

    Create BadIndentation.java

---
+++ Chapter-3/BadIndentation.java
@@ -0,0 +1,16 @@
+public class BadIndentation {
+	
+	public boolean badIndentation(int maxLines)
+	{
+		int lineCount = 3;
+		while (lineCount < maxLines)
+		{
+			System.out.println(lineCount);
+			lineCount++;
+		}
+		
+    return true;
+    
+	}
+	
+}
