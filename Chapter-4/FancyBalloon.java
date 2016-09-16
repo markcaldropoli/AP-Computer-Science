cddede6309b1d03d3a50ece02228cb227115d524
Mark Caldropoli <mcaldro1@binghamton.edu>
2016-09-15 21:17:16 -0400

    Create FancyBalloon.java

---
+++ Chapter-4/FancyBalloon.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class FancyBalloon extends Balloon
+{
+  public FancyBalloon()
+  { }
+  
+  public FancyBalloon(int x, int y, int r, Color c)
+  {
+    super(x,y,r,c);
+  }
+}
