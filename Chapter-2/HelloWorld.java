316e3b8dc4c682329ec172bad1ce5c40a7aaa9b3
Mark Caldropoli <mcaldro1@binghamton.edu>
2016-09-15 21:01:56 -0400

    Create HelloWorld.java

---
+++ Chapter-2/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * Displays "Hello World" message on the screen
+ */
+
+public class HelloWorld
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, World!");
+  }
+
+}
