a94f9e8f9b80a52751f903d0e9e14ac7b5f15d3b
Mark Caldropoli <mcaldro1@binghamton.edu>
2016-09-15 21:26:47 -0400

    Create DieTest.java

---
+++ Chapter-6/DieTest.java
@@ -0,0 +1,18 @@
+public class DieTest
+{
+  
+  public static void main(String [] args) {
+    Die die = new Die();
+    die.roll();
+    System.out.println(die.getNumDots());
+    die.roll();
+    System.out.println(die.getNumDots());
+    die.roll();
+    System.out.println(die.getNumDots());
+    die.roll();
+    System.out.println(die.getNumDots());
+    die.roll();
+    System.out.println(die.getNumDots());
+  }
+  
+}
