8718555d5f1082118c8629b737b0d5a7fd4df52d
Mark Caldropoli <mcaldro1@binghamton.edu>
2016-09-15 21:17:39 -0400

    Create OvalBalloon.java

---
+++ Chapter-4/OvalBalloon.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class OvalBalloon extends Balloon
+{
+  public OvalBalloon()
+  {  }
+  
+  public OvalBalloon(int x, int y, int r, Color c)
+  {
+    super(x,y,r,c);
+  }
+}
